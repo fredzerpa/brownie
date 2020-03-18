e01ad5d9ece18db4aef1c2ca5c8d44c552f0c974
TOR <52118178+fredzerpa@users.noreply.github.com>
2020-03-18 01:43:32 -0400

    Update CSS files and creating new Js File for Record

---
+++ js/record.js
@@ -0,0 +1,21 @@
+// JQuery Section
+$(document).ready(function () { // Sidebar Toggle Class and Content ScrollBar
+    $("#data").mCustomScrollbar({ // Thin scrollbar
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () { // Remove Sidebar and Overlay .active
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () { // Add Sidebar and Overlay .active and close dropdowns
+        $('#sidebar').toggleClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    
+});
+
